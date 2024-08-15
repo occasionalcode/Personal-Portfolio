003fd097663b2ecbdb40e5b9371b6cf34910946d
GABRIEL IBANEZ <occasionalprogrammer@gmail.com>
2024-08-15 11:57:47 +0800

    #1 feat: Added About page
    in this commit i:
    - added icons for different techstack
    - added a function for storing svg files (IconsFunctions)
    - made it responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <div className="bg-black w-screen max-w-full">
+          <div className="max-w-[1440px] mx-auto">
+            <Navbar />
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="">
       <Hero />
-      {/* <About /> */}
+      <About />
     </main>
   );
 }
